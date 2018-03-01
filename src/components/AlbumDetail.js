3a97bfc5b623c8c02dee1ff5d30b12acf8012df8
Louise Reid <l_reid@hotmail.co.uk>
2018-03-01 18:53:03 +0000

    finishing touches

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -21,7 +21,9 @@ const AlbumDetail = ({ album }) => {
         <Image source={{ uri: image }} style={styles.imageStyle} />
       </CardSection>
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)} />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
