3629cf1a52eafcd00f95670113c81c8af4cafdc3
Louise Reid <l_reid@hotmail.co.uk>
2018-03-01 17:59:19 +0000

    image styling and destructuring

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
-import CardSection from './CardSection'
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
   return (
     <Card>
       <CardSection>
         <View>
-
+          <Image source={{ uri: thumbnail_image }} style={styles.thumbnailStyle} />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -23,7 +24,11 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
-}
+};
 
 export default AlbumDetail;
